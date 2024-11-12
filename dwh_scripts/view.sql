7743d69ecdee3ea2801d2f307e6d0afe3a29d3b6
Andrew Brenev <andei.brenev@mail.ru>
2024-11-13 00:44:41 +0300

    fix views

--- dwh_scripts/view.sql
+++ dwh_scripts/view.sql
@@ -1,9 +1,11 @@
-CREATE VIEW mv_bi_companies_statistic AS
+CREATE VIEW sales_statistics.mv_bi_companies_statistic AS
 SELECT company_id, count(*) as signs_count FROM companies_activity ca WHERE event_type = 'SIGN' group by company_id;
 
 
-CREATE VIEW mv_bi_statistics as
-SELECT * FROM sales_statistics.sellers s
-  RIGHT JOIN sales_statistics.sellers_activity sa on s.id = sa.seller_id
-  RIGHT JOIN sales_statistics.tariffs t ON sa.tariff_id = t.id
- RIGHT JOIN sales_statistics.mv_bi_companies_statistic mv ON mv.company_id = sa.company_id;
+CREATE VIEW sales_statistics.mv_bi_statistics as
+SELECT *
+FROM sales_statistics.sellers_activity deal
+  RIGHT JOIN sales_statistics.sellers seller on seller.id = deal.seller_id
+  RIGHT JOIN sales_statistics.company company  on deal.company_id = company.id 
+  RIGHT JOIN sales_statistics.tariffs tariff ON deal.tariff_id = tariff.id
+  RIGHT JOIN sales_statistics.mv_bi_companies_statistic mv ON mv.company_id = deal.company_id;
