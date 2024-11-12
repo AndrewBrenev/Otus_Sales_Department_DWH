d83c2561e9ff28d37e2fcf9575f3b8efd5bb372f
Andrew Brenev <andei.brenev@mail.ru>
2024-11-13 01:23:33 +0300

    edit view

--- dwh_scripts/view.sql
+++ dwh_scripts/view.sql
@@ -5,7 +5,7 @@ SELECT company_id, count(*) as signs_count FROM companies_activity ca WHERE even
 CREATE VIEW sales_statistics.mv_bi_statistics as
 SELECT *
 FROM sales_statistics.sellers_activity deal
-  RIGHT JOIN sales_statistics.sellers seller on seller.id = deal.seller_id
-  RIGHT JOIN sales_statistics.company company  on deal.company_id = company.id 
-  RIGHT JOIN sales_statistics.tariffs tariff ON deal.tariff_id = tariff.id
-  RIGHT JOIN sales_statistics.mv_bi_companies_statistic mv ON mv.company_id = deal.company_id;
+  LEFT JOIN sales_statistics.sellers seller on seller.id = deal.seller_id
+  LEFT JOIN sales_statistics.company company  on deal.company_id = company.id 
+  LEFT JOIN sales_statistics.tariffs tariff ON deal.tariff_id = tariff.id
+  LEFT JOIN sales_statistics.mv_bi_companies_statistic mv ON mv.company_id = deal.company_id;
